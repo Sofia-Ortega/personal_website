5dcf299667a985a046674f1dec3ad440bea2bae7
Sofia-Ortega <s.ortega@tamu.edu>
2023-08-05 18:36:35 -0500

    cleaned up code

--- app.js
+++ app.js
@@ -1,56 +1,83 @@
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show')
-        }
-    })
-})
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el))
-
-
-
-// ******************** PROJECTS *********************
-function addRipple(arrayOfHtmlElements, shift) {
-  arrayOfHtmlElements.forEach((elem, index) => {
-    elem.style.setProperty('--translation-distance', `${index * shift}px` )
-    elem.style.setProperty('--rotate-deg', `0deg` )
-  })
-}
-
-function addRotation(arrayOfHtmlElements) {
-  const degrees = [1, 7, -7]
-  arrayOfHtmlElements.forEach((elem, index) => {
-    elem.style.setProperty('--rotate-deg', `${degrees[index]}deg`);
-  })
-}
-
-function removeRotation(arrayOfHtmlElements) {
-  console.log("here")
-  arrayOfHtmlElements.forEach((elem) => {
-    elem.style.setProperty('--rotate-deg', `0deg`);
-  })
-
-}
-
-function addClass(arrayOfHtmlElements, className) {
-  arrayOfHtmlElements.forEach((elem) => {
+// *********************** GLOBAL ************************
+/**
+ * Adds given class name to each of the elements in the input
+ * 
+ * @param {NodeListOf<Element>} inputElementArray - array of html elements
+ * @param {string} className - name of the class WITHOUT the dot. (eg) .my-class -> "my-class"
+ */
+function addClass(inputElementArray, className) {
+  inputElementArray.forEach((elem) => {
     elem.classList.add(className);
   })
 }
 
-function removeClass(arrayOfHtmlElements, className) {
-  arrayOfHtmlElements.forEach((elem) => {
+/**
+ * Removes given class name to each of the elements in the input
+ * 
+ * @param {NodeListOf<Element>} inputElementArray - array of html elements
+ * @param {string} className - name of the class WITHOUT the dot. (eg) .my-class -> "my-class"
+ */
+function removeClass(inputElementArray, className) {
+  inputElementArray.forEach((elem) => {
     elem.classList.remove(className);
   })
-
 }
 
 
+// *********************** INTRO **************************
+const hiddenTransitionUpElements = document.querySelectorAll('.hidden-transition-up');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show-transition-up');
+        } else {
+            entry.target.classList.remove('show-transition-up')
+        }
+    })
+})
+
+hiddenTransitionUpElements.forEach((el) => observer.observe(el))
+
+
+// ******************** PROJECTS *********************
+/**
+ * Updates translation-distancs css variable, by formula index * shift
+ * 
+ * @param {NodeListOf<Element>} inputElementArray 
+ * @param {number} shift - will set each translation-distance by index * shift pixels 
+ */
+function setTranslationDistance(inputElementArray, shift) {
+  inputElementArray.forEach((elem, index) => {
+    elem.style.setProperty('--translation-distance', `${index * shift}px` )
+  })
+}
+
+/**
+ * Sets rotate-deg css variable to each element[index] with given degree[index]
+ * Length of both inputs MUST match up
+ * 
+ * @param {NodeListOf<Element>} inputElementArray 
+ * @param {number[]} degrees - list of rotation degrees want your input to be rotated as
+ */
+function setRotationDegree(inputElementArray, degrees) {
+  inputElementArray.forEach((elem, index) => {
+    elem.style.setProperty('--rotate-deg', `${degrees[index]}deg`);
+  })
+}
+
+/**
+ * Resets rotate-deg css to 0 for each input element
+ * 
+ * @param {NodeListOf<Element>} inputElementArray 
+ */
+function resetRotationDegree(inputElementArray) {
+  inputElementArray.forEach((elem) => {
+    elem.style.setProperty('--rotate-deg', `0deg`);
+  })
+
+}
 
 
 const projectHiddenClass = "project-hidden";
@@ -61,24 +88,26 @@ const projectContainers = document.querySelectorAll('#project-list .project-cont
 
 // add initial left properties:
 const CARD_SHIFT = -375;
-addRipple(projectContainers, CARD_SHIFT);
-addRotation(projectContainers);
+const ROTATION_DEGREES = [1, 7, -7]
+setTranslationDistance(projectContainers, CARD_SHIFT);
+setRotationDegree(projectContainers, ROTATION_DEGREES);
 
 const observerProjectCards = new IntersectionObserver((entries) => {
-    console.log("observer project cards")
-    entries.forEach((entry) => {
 
+    entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            addRipple(projectContainers, 0);
+            setTranslationDistance(projectContainers, 0);
+            resetRotationDegree(projectContainers);
+
             addClass(projectContainers, 'project-show')
             removeClass(projectContainers, 'project-hidden')
-            removeRotation(projectContainers);
             
         } else {
-            addRipple(projectContainers, CARD_SHIFT );
+            setTranslationDistance(projectContainers, CARD_SHIFT );
+            setRotationDegree(projectContainers, ROTATION_DEGREES);
+            
             addClass(projectContainers, 'project-hidden')
             removeClass(projectContainers, 'project-show')
-            addRotation(projectContainers);
         }
 
     })
